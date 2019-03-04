101ba7061bb789cef4cdac6c1509715206f005a8
fuzzspot <hello@fuzzspot.com>
2019-03-04 07:18:08 -0500

    Health check to SCP random ( 5 attempts )

--- server/commands/fetchSCP.js
+++ server/commands/fetchSCP.js
@@ -1,30 +1,41 @@
 const { RichEmbed } = require('discord.js');
 
 fetchSCP = function(num, callback){
-  if (num == 'random'){
-    num = String(Math.floor(Math.random()*(4999-001+1)+001)).padStart(3,'0')
-  }
-  var link = 'http://www.scp-wiki.net/scp-' + num
-  var scpnum = 'Link -> SCP-' + num
+  var attempt = 0
+  var isRandom = false
+  fetch(num)
+  function fetch(num){
+    if (num == 'random'){
+      isRandom = true
+      num = String(Math.floor(Math.random()*(4999-001+1)+001)).padStart(3,'0')
+    }
+    var link = 'http://www.scp-wiki.net/scp-' + num
+    var scpnum = 'Link -> SCP-' + num
 
-  fetchEntry(link, num, '', function(data){
-    var embed = ''
-    if(data != 'fail'){
-      embed = new RichEmbed()
-      .setTitle(data.title)
-      .setDescription(data.description)
-      .setColor(5577355)
-      .setAuthor('Marv')
-      .setURL(data.url)
+    fetchEntry(link, num, '', function(data){
+      var embed = ''
+      if(data != 'fail'){
+        embed = new RichEmbed()
+        .setTitle(data.title)
+        .setDescription(data.description)
+        .setColor(5577355)
+        .setAuthor('Marv')
+        .setURL(data.url)
 
-      if(data.image){
-        embed.setThumbnail(data.image)
+        if(data.image){
+          embed.setThumbnail(data.image)
+        }
+
+      }else{
+        embed = "That SCP doesnt exist, use `help` for more info on the command"
+        if(attempt <= 5 && isRandom){
+          fetch('random')
+          attempt++
+          return false
+        }
       }
 
-    }else{
-      embed = "That SCP doesnt exist, use `help` for more info on the command"
-    }
-
-    callback(embed)
-  })
+      callback(embed)
+    })
+  }
 }
