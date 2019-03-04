1e51e3e1c8d22085cd4e5a0c87a8da6b998f308a
DawnTheDurr <dawnatherton123@gmail.com>
2019-03-04 00:36:58 +0000

    Add uptime to bot info

--- server/commands/fetchStats.js
+++ server/commands/fetchStats.js
@@ -1,10 +1,18 @@
 const { RichEmbed } = require('discord.js');
 
 fetchStats = function(bot, callback){
+  let totalSeconds = (bot.uptime / 1000);
+  let days = Math.floor(totalSeconds / 86400);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  let uptime = `${Math.round(days)} days, ${Math.round(hours)} hours, ${Math.round(minutes)} minutes and ${Math.round(seconds)} seconds`;
   var embed = new RichEmbed()
   .setTitle('Info')
   .setAuthor('Marv')
   .setColor(5577355)
+  .addField("Uptime", uptime)
   .addField("Guilds", bot.guilds.size)
   .addField("Channels", bot.channels.size)
   .addField("Users", bot.users.size)
