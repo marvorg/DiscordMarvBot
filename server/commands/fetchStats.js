c6ad628eeb7dc9c008b353f5b0737ecb5e5be616
AdventureCo <hello@fuzzspot.com>
2019-12-01 16:19:02 -0500

    Update info

--- server/commands/fetchStats.js
+++ server/commands/fetchStats.js
@@ -1,17 +1,17 @@
-const { RichEmbed } = require('discord.js');
+const { RichEmbed } = require('discord.js')
 
-fetchStats = function(bot, callback){
+fetchStats = function (bot, callback) {
   var uptime = timeConverter(bot.uptime)
   var embed = new RichEmbed()
-  .setTitle('Info')
-  .setAuthor('Marv')
-  .setColor(5577355)
-  .addField("Uptime", uptime)
-  .addField("Guilds", bot.guilds.size)
-  .addField("Channels", bot.channels.size)
-  .addField("Users", bot.users.size)
-  .addField("Support Server", "https://discord.gg/NEXPCJz")
-  .addField("Invite Link", "https://discordapp.com/oauth2/authorize?client_id=538173713162567690&scope=bot&permissions=125958")
+    .setTitle('Info')
+    .setAuthor('Marv')
+    .setColor(5577355)
+    .addField('Uptime', uptime)
+    .addField('Guilds This Shard', bot.guilds.size)
+    .addField('Channels This Shard', bot.channels.size)
+    .addField('Users This Shard', bot.users.size)
+    .addField('Support Server', 'https://discord.gg/NEXPCJz')
+    .addField('Invite Link', 'https://discordapp.com/oauth2/authorize?client_id=538173713162567690&scope=bot&permissions=125958')
 
   callback(embed)
 }
