31b00fe7e7eb543c204d300c2c37c42650ad757c
AdventureCo <hello@fuzzspot.com>
2019-12-01 16:11:57 -0500

    Total server count in status

--- server/bot.js
+++ server/bot.js
@@ -1,167 +1,171 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const DBL = require("dblapi.js");
-const dbl = new DBL(process.env.BOTLIST_TOKEN, bot);
-const { RichEmbed } = require('discord.js');
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const DBL = require('dblapi.js')
+const dbl = new DBL(process.env.BOTLIST_TOKEN, bot)
+const { RichEmbed } = require('discord.js')
 
 // Sets bots activity to display amount of servers
-function activity(){
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers | %help`)
+function activity () {
+  bot.shard.fetchClientValues('guilds.size')
+    .then(results => {
+      bot.user.setActivity(`Serving ${results.reduce((prev, guildCount) => prev + guildCount, 0)} servers | %help`)
+    })
+    .catch(console.error)
 }
 
 // Commands will only run after this
 bot.on('ready', () => {
-  console.log(`Started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers`);
-  activity();
+  console.log(`Started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers`)
+  activity()
   setInterval(() => {
     // lets get the total number of servers from our ShardManager
     bot.shard.fetchClientValues('guilds.size')
-    .then(results => {
-      dbl.postStats(results.reduce((prev, guildCount) => prev + guildCount, 0));
-    })
-    .catch(console.error);
-  }, 1800000);
+      .then(results => {
+        dbl.postStats(results.reduce((prev, guildCount) => prev + guildCount, 0))
+      })
+      .catch(console.error)
+  }, 1800000)
 
   // print shard info
-  setInterval(function(){
-    bot.user.setActivity(`On shard ${bot.shard.id + 1} of ${bot.shard.count}`);
-    setTimeout(function(){
+  setInterval(function () {
+    bot.user.setActivity(`On shard ${bot.shard.id + 1} of ${bot.shard.count}`)
+    setTimeout(function () {
       activity()
     }, 5000)
   }, 60000)
-});
+})
 
-//When a server is joined
+// When a server is joined
 bot.on('guildCreate', guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). With ${guild.membercount} members`);
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). With ${guild.membercount} members`)
   logGuild('Added to Guild!', guild, 'added')
-  activity();
-});
+  activity()
+})
 
-//When removed from a server
+// When removed from a server
 bot.on('guildDelete', guild => {
-  console.log(`Removed from: ${guild.name} (id: ${guild.id})`);
+  console.log(`Removed from: ${guild.name} (id: ${guild.id})`)
   logGuild('Removed from Guild!', guild, 'removed')
-  activity();
-});
+  activity()
+})
 
-//When a message is sent
+// When a message is sent
 bot.on('message', async message => {
   // Prevents replies to other bots
-  if(message.author.bot) return;
+  if (message.author.bot) return
 
-  //Finds the prefix for the guild, or uses the default if no custom prefix is set
+  // Finds the prefix for the guild, or uses the default if no custom prefix is set
   var prefix = '%'
-  if(settings.findOne({_id:message.guild.id})){
-    prefix = settings.findOne({_id:message.guild.id}).prefix
+  if (settings.findOne({ _id: message.guild.id })) {
+    prefix = settings.findOne({ _id: message.guild.id }).prefix
   }
 
-  //Replies to mentions with the servers prefix
+  // Replies to mentions with the servers prefix
   if (message.isMentioned(bot.user)) {
-    message.channel.send('Your prefix is: '+prefix);
+    message.channel.send('Your prefix is: ' + prefix)
   }
 
   // Prevents replies to un-prefixed messages
-  if(message.content.substring(0, prefix.length) != prefix) return;
+  if (message.content.substring(0, prefix.length) != prefix) return
 
-  var args = message.content.slice(prefix.length).trim().split(/ +/g);
-  var command = args.shift().toLowerCase();
+  var args = message.content.slice(prefix.length).trim().split(/ +/g)
+  var command = args.shift().toLowerCase()
 
-  switch (command){
-    case "scp":
+  switch (command) {
+    case 'scp':
 
       var num = args[0]
-      fetchSCP(num, function(data){
-        message.channel.send(data);
+      fetchSCP(num, function (data) {
+        message.channel.send(data)
       })
-      break;
+      break
 
-    case "tales":
+    case 'tales':
 
       var input = message.content.split('tales')[1].trim()
-      fetchTales(input, function(data){
-        message.channel.send(data);
+      fetchTales(input, function (data) {
+        message.channel.send(data)
       })
-      break;
+      break
 
-    case "001":
+    case '001':
 
       var proposals = fetchProposals()
-      message.channel.send(proposals);
-      break;
+      message.channel.send(proposals)
+      break
 
-    case "change_prefix":
+    case 'change_prefix':
 
       // Prevents certain users from changing the prefix in a server
-      if(message.member.hasPermission("MANAGE_GUILD")) {
+      if (message.member.hasPermission('MANAGE_GUILD')) {
         var new_prefix = args[0]
-        if(new_prefix.length >= 500){
+        if (new_prefix.length >= 500) {
           new_prefix = new_prefix.substring(0, 500)
         }
         // do we already have a prefix?
-        if(settings.findOne({_id:message.guild.id})){
+        if (settings.findOne({ _id: message.guild.id })) {
           // lets update it
-          settings.update({_id:message.guild.id}, {$set:{prefix:new_prefix}})
-        }else{
+          settings.update({ _id: message.guild.id }, { $set: { prefix: new_prefix } })
+        } else {
           // nop lets insert it
-          settings.insert({_id:message.guild.id, prefix:new_prefix})
+          settings.insert({ _id: message.guild.id, prefix: new_prefix })
         }
-        message.channel.send("Prefix set to: "+new_prefix);
-      }else{
-        message.channel.send("You need the manage guild permission to use that command!")
+        message.channel.send('Prefix set to: ' + new_prefix)
+      } else {
+        message.channel.send('You need the manage guild permission to use that command!')
       }
-      break;
+      break
 
-    case "info":
+    case 'info':
 
-      fetchStats(bot, function(data){
+      fetchStats(bot, function (data) {
         message.channel.send(data)
       })
-      break;
+      break
 
-    case "ping":
+    case 'ping':
 
       // Finds ping by checking when the 'Ping?' message is sent against when the command message was sent
-      var m = await message.channel.send('Ping?');
-      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
-      break;
+      var m = await message.channel.send('Ping?')
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`)
+      break
 
-    case "help":
+    case 'help':
 
       var page = args[0]
-      fetchHelp(page, function(data){
+      fetchHelp(page, function (data) {
         message.channel.send(data)
       })
-      break;
+      break
 
-    case "sysinfo":
+    case 'sysinfo':
 
-      fetchSysInfo(function(data){
+      fetchSysInfo(function (data) {
         message.channel.send(data)
       })
-      break;
+      break
 
-    case "generate":
+    case 'generate':
 
       var input = message.content.split('generate')[1].trim()
 
       var embed = shortPreProcessing(message)
-      var m = await message.channel.send(embed[1]);
+      var m = await message.channel.send(embed[1])
 
-      if(embed[0] != 'fail'){
-        generateShort(input, function(data){
-          m.delete();
-          if(data){
-            message.channel.send('Hey <@'+embed[2]+'> Heres your story!')
+      if (embed[0] != 'fail') {
+        generateShort(input, function (data) {
+          m.delete()
+          if (data) {
+            message.channel.send('Hey <@' + embed[2] + '> Heres your story!')
             message.channel.send(data)
-          }else{
+          } else {
             message.channel.send('Error: Must be a number ( up to 5 digits )')
           }
         })
       }
-      break;
+      break
 
-    case "purge":
+    case 'purge':
 
       var amount = ''
       var type
@@ -170,38 +174,38 @@ bot.on('message', async message => {
       var userID
       var correct_inputs = true
 
-      if(message.member.hasPermission("MANAGE_GUILD")){
+      if (message.member.hasPermission('MANAGE_GUILD')) {
         var types = ['image', 'img', 'bot']
         var user = message.mentions.users.first()
         var memb
         var amount
         var type
 
-        //Check for amount of messages to delete, and the type, by searching through all stored args
-        args.forEach(function(item, index, array){
-          if (isNaN(item) == false){
+        // Check for amount of messages to delete, and the type, by searching through all stored args
+        args.forEach(function (item, index, array) {
+          if (isNaN(item) == false) {
             amount = Number(item)
           } else if (types.includes(item) == true) {
             type = item
           }
         })
 
-        //If theres a user mention, check if theyre in the guild
-        if (user){
+        // If theres a user mention, check if theyre in the guild
+        if (user) {
           memb = message.guild.member(user)
-          if (!memb){
-            message.channel.send(`That user isn't in this server.`)
+          if (!memb) {
+            message.channel.send('That user isn\'t in this server.')
           } else {
             userID = memb.id
           }
         }
 
-        //Makes sure an amount is given and is less than or equal to 100
-        if (amount && amount <= 100){
-          purge(amount, type, userID, message, function(msgs){
-            if (msgs.size > 1){
+        // Makes sure an amount is given and is less than or equal to 100
+        if (amount && amount <= 100) {
+          purge(amount, type, userID, message, function (msgs) {
+            if (msgs.size > 1) {
               message.channel.bulkDelete(msgs)
-            } else if (msgs.size == 1){
+            } else if (msgs.size == 1) {
               var id = String(msgs.firstKey(1))
               message.channel.fetchMessage(id).then(m => m.delete())
             } else {
@@ -214,27 +218,25 @@ bot.on('message', async message => {
       } else {
         message.channel.send('You need the manage guild permission to use that command!')
       }
-      break;
+      break
 
-    case "kick":
+    case 'kick':
 
-      if(message.member.hasPermission("MANAGE_GUILD")){
+      if (message.member.hasPermission('MANAGE_GUILD')) {
         var user = message.mentions.users.first()
         var check = message.guild.member(user)
-        //If no user is mentioned
-        if (!user){
+        // If no user is mentioned
+        if (!user) {
           message.channel.send('Please specify a user to kick.')
-
         } else {
-          //If the member they try to kick is higher ranking than them
-          if (message.member.highestRole.comparePositionTo(check.highestRole) <= 0){
-            message.channel.send(`You can't kick that member`)
-
+          // If the member they try to kick is higher ranking than them
+          if (message.member.highestRole.comparePositionTo(check.highestRole) <= 0) {
+            message.channel.send('You can\'t kick that member')
           } else {
-            //Removes the mention then joins other args together for the reason
+            // Removes the mention then joins other args together for the reason
             args.shift()
             var reason = args.join(' ')
-            kickMember(check, reason, function(data){
+            kickMember(check, reason, function (data) {
               message.channel.send(data)
             })
           }
@@ -242,22 +244,22 @@ bot.on('message', async message => {
       } else {
         message.channel.send('You need the manage guild permission to use that command!')
       }
-      break;
+      break
 
-    case "ban":
+    case 'ban':
 
-      if(message.member.hasPermission("MANAGE_GUILD")){
+      if (message.member.hasPermission('MANAGE_GUILD')) {
         var user = message.mentions.users.first()
         var check = message.guild.member(user)
-        if (!user){
+        if (!user) {
           message.channel.send('Please specify a user to ban.')
         } else {
-          if (message.member.highestRole.comparePositionTo(check.highestRole) <= 0){
-            message.channel.send(`You can't ban that member.`)
+          if (message.member.highestRole.comparePositionTo(check.highestRole) <= 0) {
+            message.channel.send('You can\'t ban that member.')
           } else {
             args.shift()
             var reason = args.join(' ')
-            banMember(check, reason, function(data){
+            banMember(check, reason, function (data) {
               message.channel.send(data)
             })
           }
@@ -265,22 +267,22 @@ bot.on('message', async message => {
       } else {
         message.channel.send('You need the manage guild permission to use that command!')
       }
-      break;
+      break
 
     default:
-      message.channel.send('Thats not a valid command, use `help` to view commands.');
+      message.channel.send('Thats not a valid command, use `help` to view commands.')
   }
-});
+})
 
 // Restarts on a full disconnect, after attempting to re-login
-bot.on('disconnected', function() {
+bot.on('disconnected', function () {
   logDisconnect()
-  const exec = require("child_process").exec
-  exec("restart marv")
-});
+  const exec = require('child_process').exec
+  exec('restart marv')
+})
 
-bot.on('error', function(error){
+bot.on('error', function (error) {
   logError(error)
-});
+})
 
-bot.login(process.env.DISCORD_TOKEN);
+bot.login(process.env.DISCORD_TOKEN)
